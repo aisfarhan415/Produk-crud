e973b3b4181b24105db6d0ae87072e33c4057deb
aisfarhan <aisfarhan415@gmail.com>
2024-08-31 20:52:29 +0700

    Added Controller and Request

--- database/migrations/2024_08_31_111112_create_products_table.php
+++ database/migrations/2024_08_31_111112_create_products_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('deskripsi');
             $table->decimal('harga',8,2);
             $table->integer('jumlah_stok');
-            $table->timestamps();
         });
     }
 
